2a786c5303f7732347b0cfe3cc2046488df648a6
Takayuki Sato <sttk.xslet@gmail.com>
2025-11-12 00:50:55 +0900

    test: added integration tests (#36)

---
+++ tests/graceful_phased_cell_test.rs
@@ -0,0 +1,102 @@
+#[cfg(test)]
+mod integration_tests_of_phased_cell {
+    use setup_read_cleanup::graceful::GracefulPhasedCell;
+    use std::{error, fmt, thread, time};
+
+    #[derive(Debug)]
+    struct MyStruct {
+        vec: Vec<String>,
+    }
+    impl MyStruct {
+        const fn new() -> Self {
+            Self { vec: Vec::new() }
+        }
+        fn add(&mut self, s: String) {
+            self.vec.push(s);
+        }
+        fn clear(&mut self) {
+            self.vec.clear();
+        }
+    }
+
+    #[derive(Debug)]
+    struct MyError {}
+    impl fmt::Display for MyError {
+        fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+            write!(f, "MyError")
+        }
+    }
+    impl error::Error for MyError {}
+
+    static PHASED_CELL: GracefulPhasedCell<MyStruct> = GracefulPhasedCell::new(MyStruct::new());
+
+    #[test]
+    fn test() {
+        for i in 0..3 {
+            match PHASED_CELL.get_mut() {
+                Ok(data) => {
+                    data.add("A-".to_string() + &i.to_string());
+                }
+                Err(e) => panic!("{e:?}"),
+            }
+        }
+
+        if let Err(e) = PHASED_CELL.transition_to_read(|data| {
+            data.add("B".to_string());
+            Ok::<(), MyError>(())
+        }) {
+            panic!("{e:?}");
+        }
+
+        let mut join_handlers = Vec::<thread::JoinHandle<_>>::new();
+        for i in 0..10 {
+            let handler = thread::spawn(move || {
+                let data = PHASED_CELL.read_relaxed().unwrap();
+                assert_eq!(data.vec.as_slice().join(", "), "A-0, A-1, A-2, B",);
+                thread::sleep(time::Duration::from_millis(i * 100));
+                PHASED_CELL.finish_reading();
+            });
+            join_handlers.push(handler);
+        }
+
+        thread::sleep(time::Duration::from_millis(100));
+
+        if let Err(e) = PHASED_CELL.transition_to_cleanup(time::Duration::from_secs(5), |data| {
+            data.add("C".to_string());
+            Ok::<(), MyError>(())
+        }) {
+            panic!("{e:?}");
+        }
+
+        for i in 0..4 {
+            match PHASED_CELL.get_mut() {
+                Ok(data) => {
+                    data.add("D-".to_string() + &i.to_string());
+                }
+                Err(e) => panic!("{e:?}"),
+            }
+        }
+
+        match PHASED_CELL.get_mut() {
+            Ok(data) => {
+                assert_eq!(
+                    &data.vec,
+                    &[
+                        "A-0".to_string(),
+                        "A-1".to_string(),
+                        "A-2".to_string(),
+                        "B".to_string(),
+                        "C".to_string(),
+                        "D-0".to_string(),
+                        "D-1".to_string(),
+                        "D-2".to_string(),
+                        "D-3".to_string(),
+                    ]
+                );
+                data.clear();
+                assert_eq!(&data.vec, &[] as &[String]);
+            }
+            Err(e) => panic!("{e:?}"),
+        }
+    }
+}
---
+++ tests/phased_cell_async_test.rs
@@ -0,0 +1,159 @@
+#[cfg(test)]
+mod integration_tests_of_phased_cell_async {
+    use setup_read_cleanup::{PhasedCellAsync, PhasedErrorKind};
+    use std::{error, fmt};
+    use tokio::task;
+
+    #[derive(Debug)]
+    struct MyStruct {
+        vec: Vec<String>,
+    }
+    impl MyStruct {
+        const fn new() -> Self {
+            Self { vec: Vec::new() }
+        }
+        fn add(&mut self, s: String) {
+            self.vec.push(s);
+        }
+        fn clear(&mut self) {
+            self.vec.clear();
+        }
+    }
+
+    #[derive(Debug)]
+    struct MyError {}
+    impl fmt::Display for MyError {
+        fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+            write!(f, "MyError")
+        }
+    }
+    impl error::Error for MyError {}
+
+    static PHASED_CELL: PhasedCellAsync<MyStruct> = PhasedCellAsync::new(MyStruct::new());
+
+    #[tokio::test]
+    async fn test() {
+        let mut join_handlers = Vec::<task::JoinHandle<_>>::new();
+        for i in 0..3 {
+            let handler = tokio::spawn(async move {
+                match PHASED_CELL.lock_async().await {
+                    Ok(mut data) => {
+                        data.add("A-".to_string() + &i.to_string());
+                    }
+                    Err(e) => panic!("{e:?}"),
+                }
+            });
+            join_handlers.push(handler);
+        }
+        while join_handlers.len() > 0 {
+            let _ = match join_handlers.remove(0).await {
+                Ok(_) => Ok::<(), MyError>(()),
+                Err(e) => panic!("{e:?}"),
+            };
+        }
+
+        let mut join_handlers = Vec::<task::JoinHandle<_>>::new();
+        for i in 0..3 {
+            let handler = tokio::spawn(async move {
+                if let Err(e) = PHASED_CELL
+                    .transition_to_read_async(|data| {
+                        data.add("B-".to_string() + &i.to_string());
+                        Box::pin(async { Ok::<(), MyError>(()) })
+                    })
+                    .await
+                {
+                    assert!(
+                        e.kind == PhasedErrorKind::DuringTransitionToRead
+                            || e.kind == PhasedErrorKind::PhaseIsAlreadyRead,
+                    )
+                }
+            });
+            join_handlers.push(handler);
+        }
+        while join_handlers.len() > 0 {
+            let _ = match join_handlers.remove(0).await {
+                Ok(_) => Ok::<(), MyError>(()),
+                Err(e) => panic!("{e:?}"),
+            };
+        }
+
+        let mut join_handlers = Vec::<task::JoinHandle<_>>::new();
+        for _ in 0..10 {
+            let handler = tokio::spawn(async move {
+                let data = PHASED_CELL.read_relaxed().unwrap();
+                assert!(data.vec[0].starts_with("A-"));
+                assert!(data.vec[1].starts_with("A-"));
+                assert!(data.vec[2].starts_with("A-"));
+                assert!(data.vec[3].starts_with("B-"));
+            });
+            join_handlers.push(handler);
+        }
+        while join_handlers.len() > 0 {
+            let _ = match join_handlers.remove(0).await {
+                Ok(_) => Ok::<(), MyError>(()),
+                Err(e) => panic!("{e:?}"),
+            };
+        }
+
+        let mut join_handlers = Vec::<task::JoinHandle<_>>::new();
+        for i in 0..3 {
+            let handler = tokio::spawn(async move {
+                if let Err(e) = PHASED_CELL
+                    .transition_to_cleanup_async(|data| {
+                        data.add("C-".to_string() + &i.to_string());
+                        Box::pin(async { Ok::<(), MyError>(()) })
+                    })
+                    .await
+                {
+                    assert!(
+                        e.kind == PhasedErrorKind::PhaseIsAlreadyCleanup
+                            || e.kind == PhasedErrorKind::DuringTransitionToCleanup
+                    );
+                }
+            });
+            join_handlers.push(handler);
+        }
+        while join_handlers.len() > 0 {
+            let _ = match join_handlers.remove(0).await {
+                Ok(_) => Ok::<(), MyError>(()),
+                Err(e) => panic!("{e:?}"),
+            };
+        }
+
+        let mut join_handlers = Vec::<task::JoinHandle<_>>::new();
+        for i in 0..4 {
+            let handler = tokio::spawn(async move {
+                match PHASED_CELL.lock_async().await {
+                    Ok(mut data) => {
+                        data.add("D-".to_string() + &i.to_string());
+                    }
+                    Err(e) => panic!("{e:?}"),
+                }
+            });
+            join_handlers.push(handler);
+        }
+        while join_handlers.len() > 0 {
+            let _ = match join_handlers.remove(0).await {
+                Ok(_) => Ok::<(), MyError>(()),
+                Err(e) => panic!("{e:?}"),
+            };
+        }
+
+        match PHASED_CELL.lock_async().await {
+            Ok(mut data) => {
+                assert!(data.vec[0].starts_with("A-"));
+                assert!(data.vec[1].starts_with("A-"));
+                assert!(data.vec[2].starts_with("A-"));
+                assert!(data.vec[3].starts_with("B-"));
+                assert!(data.vec[4].starts_with("C-"));
+                assert!(data.vec[5].starts_with("D-"));
+                assert!(data.vec[6].starts_with("D-"));
+                assert!(data.vec[7].starts_with("D-"));
+                assert!(data.vec[8].starts_with("D-"));
+                data.clear();
+                assert_eq!(&data.vec, &[] as &[String]);
+            }
+            Err(e) => panic!("{e:?}"),
+        }
+    }
+}
